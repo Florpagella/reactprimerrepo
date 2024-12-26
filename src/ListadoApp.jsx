b812445051ca610848d43ad2d5ebfa6d9cfd03ac
Florencia Pagella <florencia.pagella94@gmail.com>
2024-12-26 19:37:14 -0300

    2

--- src/ListadoApp.jsx
+++ src/ListadoApp.jsx
@@ -7,22 +7,23 @@ const Items = ({ nombre, visto }) => {
 };
 
 export const ListadoApp = () => {
-  let listaDeNombres = [
-    { nombre: "Emilia", visto: true },
-    { nombre: "Florencia", visto: true },
-    { nombre: "Josefina", visto: true },
-    { nombre: "Martin", visto: false },
-    { nombre: "Marcos", visto: false },
+  let listaDeSuper = [
+    { nombre: "Aceite", visto: true },
+    { nombre: "Azucar", visto: true },
+    { nombre: "Helado", visto: true },
+    { nombre: "Manzana", visto: false },
+    { nombre: "limon", visto: false },
   ]
 
-  const [arreglo, setArreglo] = useState(listaDeNombres)
+  const [arreglo, setArreglo] = useState(listaDeSuper)
+  
   
   
   return (
     <>
-      <h1>Lista de tareas</h1>
+      <h1>Lista de Super</h1>
       <ol>
-        {arreglo.map(Items => <item nombre={Items.nombre} visto= {Items.visto} />)}
+        {arreglo.map(Item => <item nombre={Items.nombre} visto= {Items.visto} />)}
       </ol>
     </>
   );
